b1f7b9fa95353e696e991138cc23cab2c627b838
joserobjr <jose.rob.jr@gmail.com>
2014-03-20 07:16:48 -0300

    Working on extension render

--- Project Red Peripheral/src/br/com/gamemods/computercraft/integration/projectred/client/ExtensorRender.java
+++ Project Red Peripheral/src/br/com/gamemods/computercraft/integration/projectred/client/ExtensorRender.java
@@ -28,7 +28,71 @@ import codechicken.microblock.TopIconTransformation;
 import br.com.gamemods.computercraft.integration.projectred.block.ExtensorPart;
 
 public class ExtensorRender {
-	public static ModelExtensorPart model = new ModelExtensorPart();
+	//public static ModelExtensorPart model = new ModelExtensorPart();
+	public static CCModel model = null;
+	
+	public static CCModel createModel(ExtensorPart part, Vector3 pos)
+	{
+		int cubes = 4;
+		
+		int faces = cubes*6;
+        
+		CCModel model = CCModel.quadModel(faces*4);
+        
+        Icon icon = part.getIcon();
+        double  minU = icon.getMinU(),
+        		maxU = icon.getMaxU(),
+        		minV = icon.getMinV(),
+        		maxV = icon.getMaxV();
+        
+        
+        double height = 4/16D;
+        double xwidth = 6/16D;
+        double zwidth = xwidth;
+        double xpadding = 5/16D;
+        double zpadding = xpadding;
+        double ypadding = 0;
+        
+        Cuboid6 cub = new Cuboid6(0 + xpadding, 0+ypadding, 0 + zpadding, xwidth + xpadding, height+ypadding, zwidth + zpadding);
+        
+        model.generateBlock(0, cub);
+        
+        xwidth = 5/16D;
+        zwidth = 6/16D;
+        xpadding = 0;
+        zpadding = 5/16D;
+        cub = new Cuboid6(0 + xpadding, 0+ypadding, 0 + zpadding, xwidth + xpadding, height+ypadding, zwidth + zpadding);
+        model.generateBlock(1*6*4, cub);
+        
+        xwidth = 5/16D;
+        zwidth = 2/16D;
+        height = 2/16D;
+        xpadding = 11/16D;
+        zpadding = 7/16D;
+        ypadding = 0/16D;
+        cub = new Cuboid6(0 + xpadding, 0+ypadding, 0 + zpadding, xwidth + xpadding, height+ypadding, zwidth + zpadding);
+        cub.apply(new Rotation(0, 0,0,0));
+        model.generateBlock(2*6*4, cub);
+        
+        xwidth = 4/16D;
+        zwidth = 6/16D;
+        height = 4/16D;
+        xpadding = 0;
+        zpadding = 5/16D;
+        ypadding = 0/16D;
+        cub = new Cuboid6(0 + xpadding, 0+ypadding, 0 + zpadding, xwidth + xpadding, height+ypadding, zwidth + zpadding);
+        model.generateBlock(3*6*4, cub);
+        for(int i = 3*6*4; i < (3*6*4)+(6*4);i++)
+        {
+        	model.verts[i].vec.apply(new Translation(-4/16D, 0, 0));
+        }
+        
+        
+        model.apply(Rotation.sideOrientation(part.side, 0).at(Vector3.center));
+        
+        ExtensorRender.model = model;
+        return model;
+	}
 	
 	public static void render(ExtensorPart part, Vector3 pos)
 	{
@@ -36,6 +100,11 @@ public class ExtensorRender {
         CCRenderState.reset();
         CCRenderState.setBrightness(part.world(), part.x(), part.y(), part.z());
         CCRenderState.useModelColours(true);
+        
+        CCModel model = createModel(part, pos);
+
+        model.render(new Translation(pos), new IconTransformation( part.getIcon()));
+        
 		//RenderPipe.render(part, pos);
 		
 		//RenderUtils.renderItemUniform(new ItemStack(1, 1, 0));
@@ -44,9 +113,9 @@ public class ExtensorRender {
         //CCModel.quadModel(24).generateBlock(0, new Cuboid6(0, 0, 0, 10, 10, 10)).render(new Translation(new Vector3(pos.x, pos.y, pos.z)), );
 		
 		
-        int faces = 6;
+        /*int faces = 6;
         
-		CCModel model = CCModel.quadModel(faces*4);
+		CCModel model = CCModel.quadModel(faces*4);*/
 		
 		//model.apply(new IconTransformation(part.getIcon()));
 		
@@ -62,7 +131,7 @@ public class ExtensorRender {
         corners[7] = new Vector3(min.x, max.y, max.z);
         corners[6] = new Vector3(max.x, max.y, max.z);*/
         
-        int i = 0;
+        /*int i = 0;
         Vertex5[] verts = model.verts;
         
         Icon icon = part.getIcon();
@@ -75,7 +144,7 @@ public class ExtensorRender {
         
         Cuboid6 cub = new Cuboid6(0.25, 0, 0.25, 0.50, 0.1250, 0.50);
         
-        model.generateBlock(0, cub);
+        model.generateBlock(0, cub);*/
         
         /*verts[i++] = new Vertex5(corners[7], minU, minV);
         verts[i++] = new Vertex5(corners[6], maxU, minV);
@@ -131,7 +200,7 @@ public class ExtensorRender {
         
         //GL11.glPushMatrix();
         //GL11.glTranslated(part.x(), part.y(), part.z());
-        
+        /*
         TextureUtils.bindAtlas(0);
         //CCRenderState.setBrightness(part.world(), part.x(), part.y(), part.z());
         //CCRenderState.useModelColours(true);
@@ -144,7 +213,7 @@ public class ExtensorRender {
         model.apply(Rotation.sideOrientation(part.side, 0).at(Vector3.center));
         model.render(new Translation(pos), new IconTransformation( part.getIcon()));
         //model.render(new Translation(pos), new TopIconTransformation(2, new UVScale(1)));
-		
+		*/
 		/*int rotation = 0;
 		GL11.glPushMatrix();
 		GL11.glTranslatef((float)pos.x + 0.5F, (float)pos.y + 1.5F, (float)pos.z + 0.5F);
